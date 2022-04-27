f89883f203ad66118321cc51e23fb89ef0818978
YoungJae Kim <byeongal@comcom.ai>
2022-04-27 14:23:19 +0900

    dummpy code for image generation

--- api/router.py
+++ api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from api import health
+from api import health, generate
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"], prefix="/health")
+api_router.include_router(generate.router, tags=["generate"], prefix="/generate")
---
+++ models/generation.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+
+class GenerationPayload(BaseModel):
+    """
+    Payload for generating images or video.
+    """
+
+    text_prompt: str = Field(
+        # pylint: disable=line-too-long
+        "A beautiful painting of a singular lighthouse, shining its light across a tumultuous sea of blood by greg rutkowski and thomas kinkade, Trending on artstation.",
+        title="A description of what you'd like the machine to generate. Think of it like writing the caption below your image on a website.",
+    )
+
+
+class ImageGenerationResult(BaseModel):
+    """
+    summay
+    """
+
+    text_promt: str
+    result: str
