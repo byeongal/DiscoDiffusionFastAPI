83acce31ed605da6fbd5ae73bd554b130a7c75c2
YoungJae Kim <byeongal@comcom.ai>
2022-05-29 18:07:36 +0900

    make directory

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 from api.router import api_router
@@ -15,6 +16,8 @@ def get_app() -> FastAPI:
     fast_api_app.include_router(api_router, prefix=app_settings.api_prefix)
     fast_api_app.add_event_handler("startup", start_app_handler(fast_api_app))
     fast_api_app.add_event_handler("shutdown", stop_app_handler(fast_api_app))
+    if not os.path.exists("./output_images"):
+        os.makedirs("./output_images")
     return fast_api_app
 
 
